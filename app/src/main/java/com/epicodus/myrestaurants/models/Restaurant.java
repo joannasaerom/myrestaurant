89eaa57e8cbceadc331cccf4619f4d4bbb5533bc
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-11-30 13:41:07 -0800

    Finish

--- app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
@@ -2,19 +2,25 @@ package com.epicodus.myrestaurants.models;
 
 import java.util.ArrayList;
 
+import org.parceler.Parcel;
+
 /**
  * Created by Guest on 11/29/16.
  */
+
+@Parcel
 public class Restaurant {
-    private String mName;
-    private String mPhone;
-    private String mWebsite;
-    private double mRating;
-    private String mImageUrl;
-    private ArrayList<String> mAddress = new ArrayList<>();
-    private double mLatitude;
-    private double mLongitude;
-    private ArrayList<String> mCategories = new ArrayList<>();
+    String mName;
+    String mPhone;
+    String mWebsite;
+    double mRating;
+    String mImageUrl;
+    ArrayList<String> mAddress = new ArrayList<>();
+    double mLatitude;
+    double mLongitude;
+    ArrayList<String> mCategories = new ArrayList<>();
+
+    public Restaurant(){}
 
     public Restaurant(String name, String phone, String website, double rating, String imageUrl, ArrayList<String> address, double latitude, double longitude, ArrayList<String> categories){
         this.mName = name;
