26b69eea11a6969d026f54d2e6d3a0b8c79de631
Joanna Anderson and Jeremy Fryd <joanna@>
2016-12-01 10:24:10 -0800

    finish everything

--- app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
@@ -27,7 +27,7 @@ public class Restaurant {
         this.mPhone = phone;
         this.mWebsite = website;
         this.mRating = rating;
-        this.mImageUrl = imageUrl;
+        this.mImageUrl = getLargeImageUrl(imageUrl);
         this.mAddress = address;
         this.mLatitude = latitude;
         this.mLongitude = longitude;
@@ -61,5 +61,8 @@ public class Restaurant {
     public ArrayList<String> getCategories(){
         return mCategories;
     }
-
+    public String getLargeImageUrl(String imageUrl){
+        String largeImageUrl = imageUrl.substring(0, imageUrl.length() - 6).concat("o.jpg");
+        return largeImageUrl;
+    }
 }
