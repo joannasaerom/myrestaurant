ca95f2e08ada029b1c0b39fe21ea26d7d746a068
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-12-12 13:59:13 -0800

    add animation when user drags and drops saved restaurants

--- app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
@@ -3,6 +3,7 @@ package com.epicodus.myrestaurants.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,6 +12,8 @@ import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.models.Restaurant;
 import com.epicodus.myrestaurants.ui.RestaurantDetailActivity;
+import com.epicodus.myrestaurants.util.ItemTouchHelperAdapter;
+import com.epicodus.myrestaurants.util.ItemTouchHelperViewHolder;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,7 +29,7 @@ import java.util.ArrayList;
  * Created by joannaanderson on 12/4/16.
  */
 
-public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder{
+public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder{
     private static final int MAX_WIDTH = 200;
     private static final int MAX_HEIGHT = 200;
     public ImageView mRestaurantImageView;
@@ -53,30 +56,23 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder{
                 .into(mRestaurantImageView);
 
         nameTextView.setText(restaurant.getName());
-//        categoryTextView.setText(restaurant.getCategories().get(0));
         ratingTextView.setText("Rating: " + restaurant.getRating() + "/5");
     }
 
-//    @Override
-//    public void onClick(View view){
-//        final ArrayList<Restaurant> restaurants = new ArrayList<>();
-//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
-//        ref.addListenerForSingleValueEvent(new ValueEventListener(){
-//           @Override
-//            public void onDataChange(DataSnapshot dataSnapshot){
-//               for (DataSnapshot snapshot : dataSnapshot.getChildren()){
-//                   restaurants.add(snapshot.getValue(Restaurant.class));
-//               }
-//               int itemPosition = getLayoutPosition();
-//               Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
-//               intent.putExtra("position", itemPosition + "");
-//               intent.putExtra("restaurants", Parcels.wrap(restaurants));
-//               mContext.startActivity(intent);
-//           }
-//            @Override
-//            public void onCancelled(DatabaseError databaseError){
-//
-//            }
-//        });
-//    }
+    @Override
+    public void onItemSelected() {
+        itemView.animate()
+                .alpha(0.7f)
+                .scaleX(0.9f)
+                .scaleY(0.9f)
+                .setDuration(500);
+    }
+    @Override
+    public void onItemClear(){
+        itemView.animate()
+                .alpha(1f)
+                .scaleX(1f)
+                .scaleY(1f);
+    }
+
 }
---
+++ app/src/main/java/com/epicodus/myrestaurants/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,10 @@
+package com.epicodus.myrestaurants.util;
+
+/**
+ * Created by joannaanderson on 12/12/16.
+ */
+
+public interface ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
